a647a52ec28e285d74d98f341e6cdb9d3c2c64e6
Lucas <lucassetem@gmail.com>
2024-12-09 11:19:00 -0300

    feat: commit inicial do projeto

---
+++ desafio-voluntariado/src/test/java/com/db/desafio_voluntariado/DesafioVoluntariadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.desafio_voluntariado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioVoluntariadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
