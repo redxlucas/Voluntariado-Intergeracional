ff544d0a74170f18d0811c100ff63d4f48af98ce
Lucas <lucassetem@gmail.com>
2024-12-12 17:11:56 -0300

    fix: corrigido bug em que não era adicionado mais de uma atividade de interesse

--- desafio-voluntariado/src/main/java/com/db/desafio_voluntariado/entities/Usuario.java
+++ desafio-voluntariado/src/main/java/com/db/desafio_voluntariado/entities/Usuario.java
@@ -2,8 +2,8 @@ package com.db.desafio_voluntariado.entities;
 
 import java.time.LocalDate;
 import java.time.Period;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -82,7 +82,7 @@ public abstract class Usuario {
         inverseJoinColumns = @JoinColumn(name = "atividade_de_interesse_id")
     )
     @JsonManagedReference
-    private Set<AtividadeDeInteresse> atividadeDeInteresseList = new HashSet<>();
+    private List<AtividadeDeInteresse> atividadeDeInteresseList = new ArrayList<>();
 
   @PrePersist
   public void calcularIdade() {
