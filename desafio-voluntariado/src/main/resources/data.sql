1e8a6739dab03e8c9d9d80608dd35b16a27b3500
Lucas <lucassetem@gmail.com>
2024-12-16 21:21:11 -0300

    feat: inserção automática de atividades de interesse no banco de dados

---
+++ desafio-voluntariado/src/main/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO atividade_de_interesse (id, nome) VALUES
+(1, 'Aulas de informática'),
+(2, 'Acompanhamento em consultas médicas'),
+(3, 'Leitura de livros e jornais'),
+(4, 'Caminhadas e atividades físicas leves'),
+(5, 'Artesanato e pintura'),
+(6, 'Cuidado com animais de estimação'),
+(7, 'Aulas de culinária saudável'),
+(8, 'Apoio em atividades de jardinagem'),
+(9, 'Acompanhamento em compras'),
+(10, 'Oficinas de música e canto'),
+(11, 'Jogos de tabuleiro e memória'),
+(12, 'Aulas de yoga e meditação');
